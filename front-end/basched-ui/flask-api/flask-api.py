10f19a595e34a4c1203f06fd98e735064a07e70e
Schezeen Fazulbhoy <schezeenf10@gmail.com>
2020-12-02 01:35:30 +0530

    changed endpoint type to POST

--- front-end/basched-ui/flask-api/flask-api.py
+++ front-end/basched-ui/flask-api/flask-api.py
@@ -1,8 +1,18 @@
 import time
-from flask import Flask
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/crawlDir', methods = ['POST'])
+def crawlDir():
+    dirPath = request.json
+    print(dirPath)
+    return "done!"
+    #     return jsonify(name)
+    # return "No player information is given"
